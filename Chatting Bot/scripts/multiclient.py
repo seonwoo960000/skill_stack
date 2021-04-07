89ab73ef5f4b8d2ccfb62113273b3c37b0344e9b
Kim Seon Woo <seonwoo960000@naver.com>
2021-04-07 15:53:24 +0900

    Formatted multiclient.py to PEP8 standard with pylint

--- Chatting Bot/scripts/multiclient.py
+++ Chatting Bot/scripts/multiclient.py
@@ -1,3 +1,9 @@
+"""Scripts for client that can connect to a server
+(which can accept multiple clients hence the script is named multiclient.py)
+
+Features
+    * Able to receive and sned data simultaneously using thread
+"""
 import socket
 import threading
 from queue import Queue
@@ -8,72 +14,111 @@ job_queue = Queue()
 JOB_SCHEDULING = [0, 1]
 
 def recv_msg(client) :
+    """Receives a message from the server and decode it
+    :param client:
+    :return msg: decoded message
+    """
     header_length = int(client.recv(HEADER_LENGTH).decode())
     msg = client.recv(header_length).decode()
     return msg
 
-def msg_format(msg) :
-    if type(msg) != bytes : msg = msg.encode()
+def msg_format(msg):
+    """Formats a message before sending to the server
+    :param msg:
+    :return msg: msg encoded with header
+    """
+    if not isinstance(msg, bytes):
+        msg = msg.encode()
+
     header = f"{len(msg): <{HEADER_LENGTH}}".encode()
     return header + msg
 
-def connect_server(IP='localhost', PORT=9999) :
-    try :
+def connect_server(ip_='localhost', port_=9999):
+    """Returns a connected socket object using IP and PORT
+    :param ip_: ip address of the server to connect
+    :param port_: port number for the connection
+    :return socket: connected socket object(or None when connection fails)
+    """
+    try:
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect((IP, PORT))
+        client.connect((ip_, port_))
         print("Connected to the server")
         return client
-    except :
-        print("Error occurred while connecting to the server")
+    except socket.error as exception:
+        print(f"Error occurred while connecting to the server: {exception}")
         return None
 
 # scheduler tasks
-def receive_message_from_server(client) :
-    while True :
-        client.settimeout(1)
-        try :
+def receive_message_from_server(client):
+    """ Wait for the message reception from the server and print it
+    :param client: connected socket object
+    :return None:
+    """
+    while True:
+        try:
+            client.settimeout(1)
             msg_from_server = recv_msg(client)
             if len(msg_from_server) > 0:
                 print(f"\nMessage from server : {msg_from_server}")
-        except :
+        except socket.timeout as exc:
             continue
 
-def send_message_to_server(client) :
-    while True :
+def send_message_to_server(client):
+    """Send the input message to the server
+    :param client: connected socket object
+    :return None:
+    """
+    while True:
         msg_to_server = input("Enter message to send : ")
         msg_to_server = msg_format(msg_to_server)
         client.send(msg_to_server)
 
-def scheduler(client) :
-    while True :
+def scheduler(client):
+    """Schedules threads to receive and send messages
+    :param client: connected socket object
+    :return None:
+    """
+    while True:
         task_number = job_queue.get()
-        if task_number == 0 :
+        if task_number == 0:
             receive_message_from_server(client)
-        elif task_number == 1 :
+        elif task_number == 1:
             send_message_to_server(client)
 
         job_queue.task_done()
 
-def create_threads(client) :
-    for _ in range(NUMBER_OF_THREADS) :
-        t = threading.Thread(target=scheduler, args=[client])
-        t.daemon = True
-        t.start()
+def create_threads(client):
+    """create threads for scheduler
+    :param client: connected socket object
+    :return:
+    """
+    for _ in range(NUMBER_OF_THREADS):
+        thread_ = threading.Thread(target=scheduler, args=[client])
+        thread_.daemon = True
+        thread_.start()
 
-def create_tasks() :
-    for JOB in JOB_SCHEDULING :
-        job_queue.put(JOB)
+def create_tasks():
+    """Create task and put in a job queue
+    :return None:
+    """
+    for job in JOB_SCHEDULING:
+        job_queue.put(job)
 
     job_queue.join()
 
-def main() :
-    # if the server can be accessed throught the public IP, change the IP to the server's public IP 
-    client = connect_server(IP='localhost', PORT=9999)
+def main():
+    """Main function
+        * connect to server
+        * create threads and tasks
+        * after all tasks end, close the connection
+    :return:
+    """
+    client = connect_server(ip_='localhost', port_=9999)
 
     create_threads(client)
     create_tasks()
 
     client.close()
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     main()
